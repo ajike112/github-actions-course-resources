7d25af1aa74542f962878edf498877b5517f249c
yourUsername <yourEmail@example.com>
2026-05-09 17:42:40 -0500

    added GitHub Actions workflows

--- git-command.sh
+++ git-command.sh
@@ -6,6 +6,6 @@ git add . # git add command stages changes for next commit
 echo "checking status"
 git status
 git commit -am "added GitHub Actions workflows" # git commit command creates a commit that includes all staged changes
-
+# git checkout command temporarily moves to another commit
 echo "Pushing to Github Repository"
 git push -u origin master
