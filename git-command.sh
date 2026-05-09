2b210025647fcef89e27d942e87d38874c59522b
yourUsername <yourEmail@example.com>
2026-05-09 17:25:01 -0500

    added GitHub Actions workflows

--- git-command.sh
+++ git-command.sh
@@ -5,7 +5,7 @@ echo "Add files and do local commit"
 git add .
 echo "checking status"
 git status
-git commit -am "added terraform, provider, and resource blocks"
+git commit -am "added GitHub Actions workflows"
 
 echo "Pushing to Github Repository"
 git push -u origin master
