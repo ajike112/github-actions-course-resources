7d00275005fbcf6825eda0c06127a7ef1660a951
yourUsername <yourEmail@example.com>
2026-05-09 17:54:29 -0500

    added GitHub Actions workflows

--- git-command.sh
+++ git-command.sh
@@ -2,10 +2,11 @@
 #!/bin/sh
 
 echo "Add files and do local commit"
-git add . # git add command stages changes for next commit
+git add . # git add <file name> command stages changes for next commit
 echo "checking status"
 git status
 git commit -am "added GitHub Actions workflows" # git commit command creates a commit that includes all staged changes
-# git checkout command temporarily moves to another commit
+# git checkout <id> command temporarily moves to another commit
+# git revert <id> command is used to undo commits. It reevrts changes of commit by creating a new commit
 echo "Pushing to Github Repository"
 git push -u origin master
