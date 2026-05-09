ab74d01676f8328574a0fa7965b8c0a2d2f47010
yourUsername <yourEmail@example.com>
2026-05-09 18:37:44 -0500

    added GitHub Actions workflows

--- git-command.sh
+++ git-command.sh
@@ -6,7 +6,7 @@ git add . # git add <file name> command stages changes for next commit
 echo "checking status"
 git status
 git commit -am "added GitHub Actions workflows" # git commit command creates a commit that includes all staged changes
-# git checkout <id> command temporarily moves to another commit
+# git checkout -b <id> command temporarily creates a branch and checks out that branch in one step 
 # git revert <id> command is used to undo commits. It reevrts changes of commit by creating a new commit
 # git branch <name of branch> command is used to create a new branch away from the main or master branch
 # git merge <name> command is used to merge the changes from the new (e.g feature) branch to the main/master branch
